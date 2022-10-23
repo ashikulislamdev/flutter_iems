0a1dec2f40766a7b7ba234bc697cba4ead6c5713
saun2648 <ashikulislamsawan@gmail.com>
2022-10-24 04:00:28 +0600

    workig with homePage Card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,17 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_iems/helpers/constants.dart';
 import 'package:flutter_iems/widgets/common/my_appbar.dart';
 import 'package:flutter_iems/widgets/common/my_drawer.dart';
+import 'package:flutter_iems/widgets/custom_button.dart';
+import 'package:flutter_iems/widgets/home_page_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: kBackgroundColor,
-      drawer: MyDrawer(),
-      appBar: MyAppBar(),
-      
+      drawer: const MyDrawer(),
+      appBar: const MyAppBar(),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(kDefaultPadding - 4),
+        child: Column(
+          children: [
+            ManagementCard(
+              iconPath: "assets/icons/manage-income.png", 
+              cardTitle: "Income", amount: "43703 Taka",
+              onPressed: () {},
+            ),
+            const SizedBox(height: 4,),
+            ManagementCard(
+              iconPath: "assets/icons/manage-expense.png", 
+              cardTitle: "Expenses", amount: "12880 Taka",
+              onPressed: () {},
+            ),
+            const SizedBox(height: 4,),
+            ManagementCard(
+              iconPath: "assets/icons/cash.png", 
+              cardTitle: "Cash", amount: "14400 Taka",
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/home_page_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_iems/helpers/constants.dart';
+
+class ManagementCard extends StatelessWidget {
+  final String iconPath;
+  final String cardTitle;
+  final String amount;
+  final Function()? onPressed;
+  const ManagementCard({
+    Key? key,
+    required this.iconPath,
+    required this.cardTitle,
+    required this.amount, 
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: kDefaultPadding, horizontal: kDefaultPadding),
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ImageIcon(AssetImage(iconPath), size: 35, color: white,),
+              Text(cardTitle, style: kHeaderTextstyle.copyWith(color: white),),
+            ],
+          ),
+          SizedBox(height: kDefaultPadding - 6,),
+          Text(amount, style: kTitleTextstyle.copyWith(color: white, fontSize: 22),),
+          SizedBox(height: kDefaultPadding - 6,),
+          ElevatedButton(
+            onPressed: onPressed, 
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: kDefaultPadding, horizontal: kDefaultPadding * 3),
+              backgroundColor: white,
+            ),
+            child: Text("View All", style: kTitleTextstyle.copyWith(color: kPrimaryColor, fontSize: 16),),
+          )
+        ],
+      ),
+    );
+  }
+}
